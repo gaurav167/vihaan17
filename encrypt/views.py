c71923e85258904ed5d30d34c03e77e66fc91d00
gul <gulasnani7@gmail.com>
2017-10-28 01:20:38 +0530

    Created data_encrypt view for encryption

--- encrypt/views.py
+++ encrypt/views.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from Crypto.cipher import AES
+from Crypto import Random
 
 from django.shortcuts import render
+from . import forms
 
 # Create your views here.
+
+
+def data_encrypt(request):
+    form = forms.DataSaveForm(request.POST)
+
+    if form.is_valid():
+        hash_data = form.save()
+        encryption_suite = AES.new("7498211973984119", AES.MODE_CBC, Random.new().read(AES.block_size))
+        cipher_text = encryption_suite.encrypt(hash_data.data)
